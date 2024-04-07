6d64f909c46cad6ae7720d78e2803bff2c218888
I3WX <utkarshs7828@gmail.com>
2024-04-07 19:36:14 +0530

    compleated java script logic

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const gridBox = document.getElementById("gridBox");
+const gridRange = document.getElementById("gridRange");
+const gridValue = document.getElementById("gridValue");
 const btnRandom = document.getElementById("randomColor");
 const btnBlack = document.getElementById("blackColor");
 const btnClear = document.getElementById("clear");
@@ -9,11 +11,14 @@ const blueSlider = document.getElementById("blueSlider")
 const redValue = document.getElementById("redValue");   
 const greenValue = document.getElementById("greenValue");   
 const blueValue = document.getElementById("blueValue");   
-const range = document.getElementsByClassName("range")
+const ranges = document.querySelectorAll(".range")
+const colorBox = document.getElementById("colorBox"); 
+  
 
 let totalBoxes = 0;
 
 function createGrid(pix) {
+    console.log(`${pix}x${pix} Grid`)
     gridBox.innerHTML = "";
     totalBoxes = pix * pix;
     const boxWidth = 100 / pix + "%"; 
@@ -26,54 +31,93 @@ function createGrid(pix) {
         box.style.height = boxHeight;
         box.style.border = "1px solid #000"
         gridBox.appendChild(box);
+        
+        
+    
+        if (totalBoxes >= 128 * 128) {
+        box.style.border = "1px solid rgba(0,0,0,0.5)";}
+        else if (totalBoxes >= 64 * 64) {
+            box.style.border = "1px solid rgba(0,0,0,0.75)";} 
+        else if (totalBoxes >= 32 * 32) {
+            box.style.border = "1px solid rgba(0,0,0,1)";}
+        
+        }
+    modifiePixel()
+    }
+
+createGrid(3)
+
+
+
+function modifiePixel(){
+    const pixels = document.querySelectorAll(".pixel") 
+    pixels.forEach(pixel =>{
         btnRandom.addEventListener("click", function(){
-            box.addEventListener("mouseover", function() {
+            pixel.addEventListener("mouseover", function() {
                 const red = Math.round(Math.random() * 255);
                 const green = Math.round(Math.random() * 255);
                 const blue = Math.round(Math.random() * 255);
                 const color = `rgb(${red}, ${green}, ${blue})`;
-                box.style.backgroundColor = color;
-                });
-        })
-        btnBlack.addEventListener("click", function(){
-            box.addEventListener("mouseover",function(){
-                box.style.backgroundColor = "rgb(0,0,0)"
-            })
+                pixel.style.backgroundColor = color;
+            });
         })
         btnClear.addEventListener("click", function(){
-            box.style.backgroundColor = ""
+            pixel.style.backgroundColor = ""
             
         })
         btnErase.addEventListener("click", function(){
-            box.addEventListener("mouseover",function(){
-                box.style.backgroundColor = "rgb(255,255,255)"
+            pixel.addEventListener("mouseover",function(){
+                pixel.style.backgroundColor = "rgb(255,255,255)"
             })
         })
-
-        // range.addEventListener("input",function(){
-
-        // })
-
         
-        if (totalBoxes >= 128 * 128) {
-            box.style.border = "1px solid rgba(0,0,0,0.5)";}
-        else if (totalBoxes >= 64 * 64) {
-            box.style.border = "1px solid rgba(0,0,0,0.75)";
-        } else if (totalBoxes >= 32 * 32) {
-            box.style.border = "1px solid rgba(0,0,0,1)";
-        }
-    }
+        redSlider.addEventListener("input", function(){
+            pixel.addEventListener("mouseover",function(){
+                let color = `rgb(${redSlider.value},${greenSlider.value},${blueSlider.value})`
+                pixel.style.backgroundColor = color;
+            })
+        })
+        greenSlider.addEventListener("input", function(){
+            pixel.addEventListener("mouseover",function(){
+                let color = `rgb(${redSlider.value},${greenSlider.value},${blueSlider.value})`
+                pixel.style.backgroundColor = color;
+            })
+        })
+        blueSlider.addEventListener("input", function(){
+            pixel.addEventListener("mouseover",function(){
+                let color = `rgb(${redSlider.value},${greenSlider.value},${blueSlider.value})`
+                pixel.style.backgroundColor = color;
+            })
+        })
+        btnBlack.addEventListener("click", function(){
+            pixel.addEventListener("mouseover",function(){
+                pixel.style.backgroundColor = "rgb(0,0,0)"
+            })
+        })
+        
+    })
 }
-createGrid(32)
-
+    
+    
 redSlider.addEventListener("input", function() {
-    redValue.innerHTML = redSlider.value;
-});
-
+        redValue.innerHTML = redSlider.value;
+        updateColorBoxBackground()
+    });
+    
 greenSlider.addEventListener("input", function() {
-    greenValue.innerHTML = greenSlider.value;
-});
-
+        greenValue.innerHTML = greenSlider.value;
+        updateColorBoxBackground()
+    });
+    
 blueSlider.addEventListener("input", function() {
-    blueValue.innerHTML = blueSlider.value;
-});
+        blueValue.innerHTML = blueSlider.value;
+        updateColorBoxBackground()
+    });
+function updateColorBoxBackground() {
+        const color = `rgb(${redSlider.value},${greenSlider.value},${blueSlider.value})`;
+        colorBox.style.backgroundColor = color;
+    }
+gridRange.addEventListener("input",function(){
+        createGrid(gridRange.value);
+        gridValue.innerHTML = gridRange.value;
+    })
